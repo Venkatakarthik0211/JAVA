568ff5d3853a4d4f5da16b0d0cdd28ea3950b93b
Venkata Karthik <105409749+Venkatakarthik0211@users.noreply.github.com>
2023-12-23 05:06:35 +0530

    permutation added

---
+++ DSA/Recursion/Permutationandcombination.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class permutationandcombination
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine(); 
+        ArrayList<String> list = permutate("",str);
+        System.out.println(list); // prints all the permutations
+        System.out.println(list.size()); // prints the number of permutations
+        sc.close();
+    }
+    public static ArrayList<String> permutate(String processed, String unprocessed)
+    {
+        if(unprocessed.isEmpty())
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+        char ch = unprocessed.charAt(0);
+        unprocessed = unprocessed.substring(1);
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=0;i<=processed.length();i++)
+        {
+            String first = processed.substring(0,i);
+            String second = processed.substring(i);
+            list.addAll(permutate(first+ch+second,unprocessed));
+        }
+        return list;
+    }
+
+}
